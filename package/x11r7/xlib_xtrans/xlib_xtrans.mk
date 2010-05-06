d8898e11e79a75cbaa5d7ece86254603e391510b
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-05-06 20:07:29 +0200

    Enable xapp_bdftopcf dependencies for the host
    Now that xapp_bdftopcf is enabled on the host, we also need its
    dependencies to be available for the host. The dependency of
    host-xapp_bdftopcf is host-xlib_libXfont, which itself depends on a
    bunch of other packages. Some of them were already available for the
    host, some not (xfont_encodings, xlib_xtrans, xproto_fontcacheproto,
    xproto_fontsproto) and are therefore added by this patch.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_xtrans/xlib_xtrans.mk
+++ package/x11r7/xlib_xtrans/xlib_xtrans.mk
@@ -12,3 +12,4 @@ XLIB_XTRANS_INSTALL_STAGING = YES
 XLIB_XTRANS_CONF_OPT = --enable-shared --disable-static
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_xtrans))
+$(eval $(call AUTOTARGETS,package/x11r7,xlib_xtrans,host))
