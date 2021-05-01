e538836a13fd892e2f727bb875922c19f42aae56
Mikhail B <wproxym@at.gmail>
2021-05-01 11:34:05 +0300

    revert cdrtools/mkisofs iso9660 to 2.01 buggy (conflicting types for 'fexecve' and 'getline' + grub rootfs.i686.ext2 'no such file')

--- target/iso9660/iso9660.mk
+++ target/iso9660/iso9660.mk
@@ -3,10 +3,10 @@
 # mkisofs to build to target iso9660 filesystems
 #
 #############################################################
-MKISOFS_SOURCE:=cdrtools-2.01.01a69.tar.bz2
+MKISOFS_SOURCE:=cdrtools-2.01.tar.bz2
 MKISOFS_CAT:=$(BZCAT)
-MKISOFS_SITE:=ftp://ftp.berlios.de/pub/cdrecord/alpha
-MKISOFS_DIR:=$(BUILD_DIR)/cdrtools-2.01.01
+MKISOFS_SITE:=ftp://ftp.berlios.de/pub/cdrecord/
+MKISOFS_DIR:=$(BUILD_DIR)/cdrtools-2.01
 MKISOFS_TARGET=$(MKISOFS_DIR)/mkisofs/OBJ/$(HOST_ARCH)-linux-cc/mkisofs
 
 $(DL_DIR)/$(MKISOFS_SOURCE):
