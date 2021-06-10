bcccc680b5ccbb4676e17a8b15cab0ee5f90e12f
Mikhail B <wproxym@at.gmail>
2021-06-10 21:04:33 +0300

    xserver_xorg-server: fix _MAKE typo: partial

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -7,38 +7,37 @@
 XSERVER_XORG_SERVER_VERSION = 1.5.3
 XSERVER_XORG_SERVER_SOURCE = xorg-server-$(XSERVER_XORG_SERVER_VERSION).tar.bz2
 XSERVER_XORG_SERVER_SITE = http://xorg.freedesktop.org/releases/individual/xserver
+XSERVER_XORG_SERVER_MAKE = $(MAKE1) # make install fails with parallel make
 XSERVER_XORG_SERVER_AUTORECONF = NO
+XSERVER_XORG_SERVER_LIBTOOL_PATCH = NO
 XSERVER_XORG_SERVER_INSTALL_STAGING = YES
 XSERVER_XORG_SERVER_USE_CONFIG_CACHE = NO # overrides CFLAGS
 XSERVER_XORG_SERVER_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install install-data
 
-XSERVER_XORG_SERVER_DEPENDENCIES = 	\
-	xutil_util-macros 		\
-	xlib_libXfont 			\
-	xlib_libX11 			\
-	xlib_libXau 			\
-	xlib_libXdmcp 			\
-	xlib_libXScrnSaver 		\
-	xlib_libXext 			\
-	xlib_libXfixes 			\
-	xlib_libXi 			\
-	xlib_libXrender 		\
-	xlib_libXres 			\
-	xlib_libXft 			\
-	xlib_libXcursor 		\
-	xlib_libXinerama 		\
-	xlib_libXrandr 			\
-	xlib_libXdamage 		\
-	xlib_libXxf86misc 		\
-	xlib_libXxf86vm 		\
-	xlib_liblbxutil 		\
-	xlib_libxkbfile 		\
-	xlib_xtrans 			\
-	xdata_xbitmaps 			\
-	xproto_bigreqsproto 		\
-	xproto_compositeproto 		\
+XSERVER_XORG_SERVER_DEPENDENCIES =     \
+	xutil_util-macros               \
+	xlib_libpciaccess               \
+	xlib_libXfont                   \
+	xlib_libX11                     \
+	xlib_libXau                     \
+	xlib_libXdmcp                   \
+	xlib_libXScrnSaver              \
+	xlib_libXext                    \
+	xlib_libXfixes                  \
+	xlib_libXi                      \
+	xlib_libXinerama                \
+	xlib_libXrandr                  \
+	xlib_libXdamage                 \
+	xlib_libXxf86misc               \
+	xlib_libXxf86vm                 \
+	xlib_liblbxutil                 \
+	xlib_libxkbfile                 \
+	xlib_xtrans                     \
+	xdata_xbitmaps                  \
+	xproto_bigreqsproto             \
+	xproto_compositeproto           \
 	xproto_damageproto 		\
-	xproto_evieext 			\
+	xproto_evieext 		\
 	xproto_fixesproto 		\
 	xproto_fontsproto 		\
 	xproto_glproto 			\
@@ -51,9 +50,9 @@ XSERVER_XORG_SERVER_DEPENDENCIES = 	\
 	xproto_xcmiscproto 		\
 	xproto_xextproto 		\
 	xproto_xf86bigfontproto 	\
-	xproto_xf86dgaproto 		\
-	xproto_xf86driproto	 	\
-	xproto_xf86miscproto 		\
+	xproto_xf86dgaproto             \
+	xproto_xf86driproto             \
+	xproto_xf86miscproto            \
 	xproto_xf86rushproto 		\
 	xproto_xf86vidmodeproto 	\
 	xproto_xproto 			\
@@ -68,8 +67,7 @@ XSERVER_XORG_SERVER_CONF_OPT = --disable-config-hal \
 		CFLAGS="-I$(STAGING_DIR)/usr/include/pixman-1"
 
 ifeq ($(BR2_PACKAGE_XSERVER_xorg),y)
-XSERVER_XORG_SERVER_DEPENDENCIES += mesa3d
-XSERVER_XORG_SERVER_CONF_OPT += --with-mesa-source="$(BUILD_DIR)/Mesa-$(MESA3D_VERSION)" --enable-xorg
+XSERVER_XORG_SERVER_CONF_OPT += --enable-xorg --disable-glx
 else
 XSERVER_XORG_SERVER_CONF_OPT += --disable-xorg
 endif
@@ -80,16 +78,16 @@ else
 XSERVER_XORG_SERVER_CONF_OPT += --disable-kdrive --disable-xfbdev
 endif
 
+ifeq ($(BR2_PACKAGE_MESA3D),y)
+XSERVER_XORG_SERVER_DEPENDENCIES += mesa3d xproto_xf86driproto
+else
+XSERVER_XORG_SERVER_CONF_OPT += --disable-dri
+endif
+
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER_NULL_CURSOR),y)
 XSERVER_XORG_SERVER_CONF_OPT += --enable-null-root-cursor
 else
 XSERVER_XORG_SERVER_CONF_OPT += --disable-null-root-cursor
-endif
-
-ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER_BUILTIN_FONTS),y)
-XSERVER_XORG_SERVER_CONF_OPT += --enable-builtin-fonts
-else
-XSERVER_XORG_SERVER_CONF_OPT += --disable-builtin-fonts
 endif
 
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER_AIGLX),y)
@@ -111,17 +109,35 @@ endif
 
 ifeq ($(BR2_PACKAGE_FREETYPE),y)
 XSERVER_XORG_SERVER_DEPENDENCIES += freetype
-XSERVER_XORG_SERVER_CONF_OPT += --enable-freetype
-endif
-
-ifeq ($(BR2_PACKAGE_XPROTO_TRAPPROTO),y)
-XSERVER_XORG_SERVER_DEPENDENCIES += xproto_trapproto
-XSERVER_XORG_SERVER_CONF_OPT += --enable-xtrap
 endif
 
 ifeq ($(BR2_PACKAGE_XPROTO_RECORDPROTO),y)
 XSERVER_XORG_SERVER_DEPENDENCIES += xproto_recordproto
 XSERVER_XORG_SERVER_CONF_OPT += --enable-record
+endif
+
+ifneq ($(BR2_PACKAGE_XLIB_LIBXVMC),y)
+XSERVER_XORG_SERVER_CONF_OPT += --disable-xvmc
+endif
+
+ifneq ($(BR2_PACKAGE_XLIB_LIBXCOMPOSITE),y)
+XSERVER_XORG_SERVER_CONF_OPT += --disable-composite
+endif
+
+ifeq ($(BR2_PACKAGE_XPROTO_DRI2PROTO),y)
+XSERVER_XORG_SERVER_DEPENDENCIES += xproto_dri2proto
+XSERVER_XORG_SERVER_CONF_OPT += --enable-dri2
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXSCRNSAVER),y)
+XSERVER_XORG_SERVER_DEPENDENCIES += xlib_libXScrnSaver
+XSERVER_XORG_SERVER_CONF_OPT += --enable-screensaver
+else
+XSERVER_XORG_SERVER_CONF_OPT += --disable-screensaver
+endif
+
+ifneq ($(BR2_PACKAGE_XLIB_LIBDMX),y)
+XSERVER_XORG_SERVER_CONF_OPT += --disable-dmx
 endif
 
 
