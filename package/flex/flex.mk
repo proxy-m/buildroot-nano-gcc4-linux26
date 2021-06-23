5bc751af6787a4c52214a0297b3ccad247d6e169
Mikhail B <wproxym@at.gmail>
2021-06-24 02:28:43 +0300

    flex: bump version 2.5.35

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -3,13 +3,16 @@
 # flex
 #
 #############################################################
-FLEX_VERSION:=2.5.33
-FLEX_PATCH_VERSION:=11
+FLEX_VERSION:=2.5.35
+FLEX_PATCH_VERSION:=9.1
 FLEX_SOURCE:=flex_$(FLEX_VERSION).orig.tar.gz
 FLEX_PATCH:=flex_$(FLEX_VERSION)-$(FLEX_PATCH_VERSION).diff.gz
 FLEX_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/f/flex
 FLEX_DIR:=$(BUILD_DIR)/flex-$(FLEX_VERSION)
 FLEX_INSTALL_STAGING=YES
+FLEX_DEPENDENCIES = \
+	$(if $(BR2_PACKAGE_GETTEXT),gettext) \
+	$(if $(BR2_PACKAGE_LIBINTL),libintl)
 
 # lex -> flex
 define FLEX_INSTALL_LEX
@@ -38,4 +41,3 @@ FLEX_POST_CLEAN_HOOKS += FLEX_UNINSTALL_LIBFL
 endif
 
 $(eval $(call AUTOTARGETS,package,flex))
-$(eval $(call AUTOTARGETS,package,flex,host))
