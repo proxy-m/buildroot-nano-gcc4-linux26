058f03e3005a53574cace8f8835eddaa46796eac
Mikhail B <wproxym@at.gmail>
2021-06-26 13:02:18 +0300

    add xproto_dri2proto: like 864950c1a4a873ef0e26025035259c888c0da178

--- package/x11r7/xcb-util/xcb-util.mk
+++ package/x11r7/xcb-util/xcb-util.mk
@@ -3,10 +3,11 @@
 # xcb-util
 #
 #############################################################
-XCB_UTIL_VERSION = 0.3.6
+XCB_UTIL_VERSION = 0.2
 XCB_UTIL_SOURCE = xcb-util-$(XCB_UTIL_VERSION).tar.bz2
 XCB_UTIL_SITE = http://xcb.freedesktop.org/dist/
-XCB_UTIL_LIBTOOL_PATCH = NO
+
+XCB_UTIL_DEPENDENCIES += gperf
 
 $(eval $(call AUTOTARGETS,package/x11r7,xcb-util))
 
