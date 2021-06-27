a595eaffd5817122172955724a12de2e5829e6e5
Mikhail B <wproxym@at.gmail>
2021-06-27 10:56:48 +0300

    small update

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -3,32 +3,27 @@
 # libxcb
 #
 #############################################################
-LIBXCB_VERSION = 1.5
+LIBXCB_VERSION = 1.7
 LIBXCB_SOURCE = libxcb-$(LIBXCB_VERSION).tar.bz2
 LIBXCB_SITE = http://xcb.freedesktop.org/dist/
 
 LIBXCB_INSTALL_STAGING = YES
-
-LIBXCB_AUTORECONF = NO
 LIBXCB_LIBTOOL_PATCH = NO
-LIBXCB_DEPENDENCIES = host-pkgconfig libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau
+
+## TODO: FIX!!! Python on your host must be same as in buildroot. Temporary limitation.
+###HOST_PYTHON_FULL := "$(shell echo unshare -mr chroot $(TMP_TMP__DIR) /usr/bin/python$(HOST_PYTHON_VERSION))"
+
+##HOST_PYTHON_VERSION=2.7
+HOST_PYTHON_VERSION=$(shell python --version 2>&1 | sed 's/Python \([0-9]\.[^\.]\).*/\1/')
+
+LIBXCB_DEPENDENCIES = \
+	libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau \
+	xcb-proto host-pkgconfig
 LIBXCB_CONF_ENV = STAGING_DIR="$(STAGING_DIR)"
+LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb \
+	XCBPROTO_XCBPYTHONDIR=$(TARGET_DIR)/usr/lib/python$(HOST_PYTHON_VERSION)/site-packages
+LIBXCB_CONF_OPT = --disable-build-docs
 
-#####HOST_PYTHON_VERSION=$(shell python --version 2>&1 | sed 's/Python \([0-9]\.[^\.]\).*/\1/')
-HOST_PYTHON_VERSION=2.7
-
-## TODO: FIX!!!
-#HOST_PYTHON_0 := ""
-#HOST_PYTHON_0 := $(shell cp -a $(TARGET_DIR)/usr/bin/python$(HOST_PYTHON_VERSION) $(TARGET_DIR)/usr/bin/python  $(STAGING_DIR)/usr/bin/)
-#HOST_PYTHON_0 := $(shell cp -a $(TARGET_DIR)/usr/lib/libpython$(HOST_PYTHON_VERSION)* $(STAGING_DIR)/usr/lib/)
-#HOST_PYTHON_0 := $(shell cp -a $(TARGET_DIR)/usr/lib/lib* $(STAGING_DIR)/usr/lib/)
-#HOST_PYTHON_0 := $(shell cp -a $(TARGET_DIR)/usr/lib/python$(HOST_PYTHON_VERSION) $(STAGING_DIR)/usr/lib/)
-
-HOST_PYTHON_FULL := "$(shell echo unshare -mr chroot $(STAGING_DIR) /usr/bin/python$(HOST_PYTHON_VERSION))"
-
-LIBXCB_MAKE_OPT = XCBPROTO_PREFIX_STAGING_DIR=$(STAGING_DIR) XCBPROTO_XCBINCLUDEDIR=/usr/share/xcb PYTHON=$(HOST_PYTHON_FULL) XCBPROTO_XCBPYTHONDIR=/usr/lib/python$(HOST_PYTHON_VERSION)/site-packages
-###LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb XSLTPROC=/usr/bin/xsltproc
-#####LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb XSLTPROC=/bin/true XCBPROTO_XCBPYTHONDIR=/dev/null
+HOST_LIBXCB_CONF_OPT = --disable-build-docs
 
 $(eval $(call AUTOTARGETS,package/x11r7,libxcb))
-
