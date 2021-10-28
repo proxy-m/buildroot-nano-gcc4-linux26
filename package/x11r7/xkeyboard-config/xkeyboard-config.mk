a5b4623db6a437410734ee3b7467b9bd7d33dce5
Mikhail B <wproxym@at.gmail>
2021-10-29 01:16:05 +0300

    x11 drivers: video and input version bump

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -5,7 +5,7 @@
 #############################################################
 XKEYBOARD_CONFIG_VERSION = 1.2
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
-XKEYBOARD_CONFIG_SITE = http://xlibs.freedesktop.org/xkbdesc
+XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config/
 XKEYBOARD_CONFIG_AUTORECONF = NO
 XKEYBOARD_CONFIG_INSTALL_STAGING = NO
 XKEYBOARD_CONFIG_INSTALL_TARGET = YES
@@ -13,7 +13,7 @@ XKEYBOARD_CONFIG_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 XKEYBOARD_CONFIG_CONF_OPT = GMSGFMT=/usr/bin/msgfmt
 
-XKEYBOARD_CONFIG_DEPENDENCIES = uclibc
+XKEYBOARD_CONFIG_DEPENDENCIES = uclibc gettext
 
 $(eval $(call AUTOTARGETS,package/x11r7,xkeyboard-config))
 
