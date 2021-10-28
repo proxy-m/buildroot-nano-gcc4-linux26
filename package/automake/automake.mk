83614ea7d88c0dcb44c4649a30c03408b5bae526
Mikhail B <wproxym@at.gmail>
2021-10-28 16:21:17 +0300

    fix: automake + autoconf + m4

--- package/automake/automake.mk
+++ package/automake/automake.mk
@@ -12,7 +12,7 @@ ifeq ($(BR2_ENABLE_DEBUG),y) # install-exec doesn't install aclocal stuff
 AUTOMAKE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 endif
 
-AUTOMAKE_DEPENDENCIES = host-autoconf microperl
+AUTOMAKE_DEPENDENCIES = autoconf microperl
 
 $(eval $(call AUTOTARGETS,package,automake))
 
